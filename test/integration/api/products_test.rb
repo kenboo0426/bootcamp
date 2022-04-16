7cfa34297e023ac907e51aab446ad11a0557b2c9
Aseiide <eiseiide@gmail.com>
2022-04-16 18:42:56 +0900

    integrationテストを実装

--- test/integration/api/products_test.rb
+++ test/integration/api/products_test.rb
@@ -54,4 +54,14 @@ class API::ProductsTest < ActionDispatch::IntegrationTest
     actual = response.parsed_body['products'].map { |product| product['practice']['title'] }
     assert_equal expected, actual
   end
+
+  test 'return correct number of products' do
+    token = create_token('komagata', 'testtest')
+    get api_products_unassigned_index_path(format: :json),
+        headers: { 'Authorization' => "Bearer #{token}" }
+
+    expected = products(:product8, :product10, :product11, :product12, :product13, :product14).map.count
+    actual = response.parsed_body['products_grouped_by_elapsed_days'].find { |i| i['elapsed_days'] == 7 }['products'].count
+    assert_equal expected, actual
+  end
 end
